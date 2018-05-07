d7407d9417a3b43dffd6f76966fb40a5b62163fe
Christian <c.atallah2@newcastle.ac.uk>
2018-05-07 18:09:48 +0100

    Can now deal with multiple usages

--- produce_sbol_provo_graph.py
+++ produce_sbol_provo_graph.py
@@ -5,7 +5,9 @@ from rdflib import Graph, URIRef
 from graphviz import Digraph
 
 g = Graph()
-g.parse('./sbol2.xml', 'rdfxml')
+# g.parse('./design_build_test.xml', 'rdfxml')
+g.parse('./full_life_cycle.xml', 'rdfxml')
+# g.parse('./sbol2.xml', 'rdfxml')
 
 dot = Digraph('design_build_test')
 
@@ -73,7 +75,7 @@ for (s, p, o) in sorted(g):
 
                 temp_label = s.split('/')[-1]
 
-            elif cur_class == 'Implementation' or cur_class == 'Test' or cur_class == 'Model' or cur_class == 'Activity' or cur_class =='Plan':
+            elif cur_class == 'Implementation' or cur_class == 'ComponentDefinition' or cur_class == 'ModuleDefinition' or cur_class == 'Test' or cur_class == 'Model' or cur_class == 'Activity' or cur_class =='Plan':
 
                 for displayId in g.objects(URIRef(s), URIRef('http://sbols.org/v2#displayId')):
                     temp_label =  displayId
@@ -116,10 +118,21 @@ for (s, p, o) in sorted(g):
                     for activity in g.subjects(URIRef('http://www.w3.org/ns/prov#qualifiedUsage'), URIRef(s)):
 
                         for entity in g.objects(URIRef(s), URIRef('http://www.w3.org/ns/prov#entity')):
-                            activity_entity[activity.split(':')[-1]] = entity.split(':')[-1]
+
+                            if activity.split(':')[-1] in activity_entity.keys():
+                                activity_entity[activity.split(':')[-1]] += [entity.split(':')[-1]]
+
+                            else:
+
+                                activity_entity[activity.split(':')[-1]] = [entity.split(':')[-1]]
 
                         for role in g.objects(URIRef(s), URIRef('http://www.w3.org/ns/prov#hadRole')):
-                            activity_usg_roles[activity.split(':')[-1]] = 'role_' + role.split('#')[-1]
+
+                            if activity.split(':')[-1] in activity_usg_roles.keys():
+                                activity_usg_roles[activity.split(':')[-1]] += ['role_' + role.split('/')[-1].split('#')[-1]]
+
+                            else:
+                                activity_usg_roles[activity.split(':')[-1]] = ['role_' + role.split('/')[-1].split('#')[-1]]
 
 
 
@@ -139,13 +152,20 @@ for (s, p, o) in sorted(g):
 
 
 for activity in activity_usg_roles.keys(): # Create edges from activity to its usage's entity
-    edges += [(activity, activity_entity[activity], activity_usg_roles[activity])]
+
+    for i in range(0, len(activity_entity[activity])):
+        edges += [(activity, activity_entity[activity][i], activity_usg_roles[activity][i])]
 
 for activity in activity_agent.keys(): # Create edges from activity to its association's agent
     edges += [(activity, activity_agent[activity], 'agent')]
 
 for activity in activity_plan.keys(): # Create edges from activity to its association's plan
     edges += [(activity, activity_plan[activity], 'plan')]
+
+
+
+
+print nodes
 
 for node in nodes: # Create the nodes
 
@@ -158,8 +178,6 @@ for node in nodes: # Create the nodes
     else:
         dot.node(node[0], node[1], shape = node[2])
 
-
-print node_names
 for edge in edges: # Create the edges
 
     if edge[0] not in node_names:
@@ -178,4 +196,6 @@ for edge in edges: # Create the edges
 
 print dot.source
 
-dot.render('sbol2', view=True) # Create the graph image
+# dot.render('design_build_test', view=True) # Create the graph image
+dot.render('full_life_cycle', view=True) # Create the graph image
+# dot.render('sbol2', view=True) # Create the graph image
