6275abd921ffa7a73783d14a78c3b699b9f93aab
Christian <c.atallah2@newcastle.ac.uk>
2018-05-07 17:26:56 +0100

    Fixed some bugs

--- produce_sbol_provo_graph.py
+++ produce_sbol_provo_graph.py
@@ -5,7 +5,7 @@ from rdflib import Graph, URIRef
 from graphviz import Digraph
 
 g = Graph()
-g.parse('./design_build_test.xml', 'rdfxml')
+g.parse('./sbol2.xml', 'rdfxml')
 
 dot = Digraph('design_build_test')
 
@@ -45,6 +45,7 @@ colour_dict = { # Colour Dict
     'ModuleDefinition': 'darkseagreen',
     'Implementation': 'darkseagreen1',
     'Test': 'darkseagreen2',
+    'Attachment': 'darkseagreen2',
     'Model': 'darkseagreen3'
 
 }
@@ -68,29 +69,32 @@ for (s, p, o) in sorted(g):
             cur_class = value.split('#')[-1] # Track the current class
 
             # Different labels depending on the class
-            if cur_class == 'Agent' or cur_class =='Plan':
+            if cur_class == 'Agent':
 
                 temp_label = s.split('/')[-1]
 
-            elif cur_class == 'Implementation' or cur_class == 'Test' or cur_class == 'Model' or cur_class == 'Activity':
-                temp_label = '/'.join(s.split('/')[-2:])
+            elif cur_class == 'Implementation' or cur_class == 'Test' or cur_class == 'Model' or cur_class == 'Activity' or cur_class =='Plan':
 
-            else:
-                temp_label = s
+                for displayId in g.objects(URIRef(s), URIRef('http://sbols.org/v2#displayId')):
+                    temp_label =  displayId
 
             node_names += [s]
+
             node_dict[s] = s.split(':')[-1]
 
             if cur_class in shape_dict.keys() or cur_class in colour_dict:
 
-                if cur_class == 'Agent' or cur_class == 'Plan':
+                if cur_class == 'Agent':
                     nodes += [(s.split('/')[-1], temp_label, shape_dict[cur_class])]
 
+                elif cur_class == 'Plan':
+                    nodes += [(s.split(':')[-1], temp_label, shape_dict[cur_class])]
+
                 elif cur_class == 'ComponentDefinition' or cur_class == 'ModuleDefinition' or cur_class == 'Implementation' or cur_class == 'Test' or cur_class == 'Model':
-                    print 'HIHIHIHI'
                     nodes += [(s.split(':')[-1], temp_label, colour_dict[cur_class], 'entity')]
 
-                elif cur_class != 'Usage' and cur_class != 'Association':
+                elif cur_class != 'Usage' and cur_class != 'Association' and cur_class != 'Attachment':
+                    # print temp_label
                     nodes += [(s.split(':')[-1], temp_label, shape_dict[cur_class])]
 
                 if cur_class == 'Association':
@@ -104,7 +108,7 @@ for (s, p, o) in sorted(g):
                             activity_agent[activity.split(':')[-1]] = agent.split('/')[-1]
 
                         for plan in g.objects(URIRef(s), URIRef('http://www.w3.org/ns/prov#hadPlan')):
-                            activity_plan[activity.split(':')[-1]] = plan.split('/')[-1]
+                            activity_plan[activity.split(':')[-1]] = plan.split(':')[-1]
 
 
                 elif cur_class == 'Usage':
@@ -118,15 +122,20 @@ for (s, p, o) in sorted(g):
                             activity_usg_roles[activity.split(':')[-1]] = 'role_' + role.split('#')[-1]
 
 
-            else:
-                dot.node(s.split(':')[-1], temp_label)
-
 
     if p == 'http://www.w3.org/ns/prov#wasDerivedFrom':
-        edges += [(s, o, 'wasDerivedFrom')]
+
+        for value in g.objects(URIRef(o), URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#type')):
+
+            cur_class = value.split('#')[-1]
+
+            if cur_class == 'Attachment':
+                nodes += [(o.split(':')[-1], o.split('/')[-2], colour_dict[cur_class], 'entity')]
+
+        edges += [(s.split(':')[-1], o.split(':')[-1], 'wasDerivedFrom')]
 
     elif p == 'http://www.w3.org/ns/prov#wasGeneratedBy':
-        edges += [(s, o, 'wasGeneratedBy')]
+        edges += [(s.split(':')[-1], o.split(':')[-1], 'wasGeneratedBy')]
 
 
 for activity in activity_usg_roles.keys(): # Create edges from activity to its usage's entity
@@ -144,28 +153,29 @@ for node in nodes: # Create the nodes
         dot.node(node[0], node[1], shape = node[2], style='filled', fillcolor = colour_dict[activity_asc_roles[node[0]]])
 
     elif len(node) == 4:
-        print 'hiiii'
         dot.node(node[0], node[1], style='filled', fillcolor = node[2])
 
     else:
         dot.node(node[0], node[1], shape = node[2])
 
 
+print node_names
 for edge in edges: # Create the edges
 
     if edge[0] not in node_names:
-
+        print edge[0]
+        node_names += [edge[0]]
         dot.node(edge[0].split(':')[-1], '/'.join(edge[0].split('/')[-2:]))
         node_dict[edge[0]] = edge[0].split(':')[-1]
 
     if edge[1] not in node_names:
-
+        print edge[1]
+        node_names += [edge[1]]
         node_dict[edge[1]] = edge[1].split(':')[-1]
         dot.node(edge[1].split(':')[-1], '/'.join(edge[1].split('/')[-2:]))
 
     dot.edge(node_dict[edge[0]], node_dict[edge[1]], str(edge[2]))
 
-
 print dot.source
 
-dot.render('design_build_test', view=True) # Create the graph image
+dot.render('sbol2', view=True) # Create the graph image
